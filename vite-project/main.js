27cd426935caca48777a78fd9b037f29a6e13df2
Sean <conk3r_xd@hotmail.com>
2022-02-13 22:48:50 +0100

    asdasd

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,12 @@
 import './style.css'
+import './scraperComponent.js';
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+<h1>Hello Vite!</h1>
+<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+
+const scraper = document.querySelector('#scraper')
+scraper.test();
+console.log("index.js")
