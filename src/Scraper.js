86b2f672071b35cb722f7ce1cc109a59aff58197
Sean <conk3r_xd@hotmail.com>
2022-07-18 07:54:22 +0200

    split out data and stuff for parallesim

--- src/Scraper.js
+++ src/Scraper.js
@@ -1,6 +1,4 @@
 import { JSDOM } from 'jsdom';
-//import datas from './data/data-test.json'
-import datas from './data/data.json' assert {type: 'json'};
 import ScraperUtils from './ScraperUtils.js';
 
 const actionType = 'Auction'
@@ -8,9 +6,7 @@ const sortBy = 'sortBy=TimeLeft'
 const linkPrefix = 'www.tradera.com'
 
 export default class Scraper {
-
-	constructor(page) {
-		this.page = page
+	constructor() {
 	}
 
 	getURL(name, auctionType) {
@@ -18,7 +14,156 @@ export default class Scraper {
 		return `https://www.tradera.com/search?q=${name}&itemType=${auctionType}&${sortBy}`;
 	}
 
-	printStart() {
+	async Scrape(data, page){
+		if (data.ignore === "true") {
+			return;
+		}
+
+		let url = this.getURL(data.searchterm, actionType);
+		console.log(url)
+
+		await ScraperUtils.setCookiesInBrowser(page)
+		await page.goto(url);
+		await page.waitForSelector('.site-pagename-SearchResults ');
+
+		//await page.waitForTimeout(50000)
+
+		await ScraperUtils.removeGDPRPopup(page)
+
+		//page down
+		for (let i = 0; i < 20; i++) {
+			await page.keyboard.press("PageDown");
+			await page.waitForTimeout(50)
+		}
+
+		//#region click wishbutton
+		let result = await page.$$('[aria-label="Spara i minneslistan"]');
+
+		for (const element of result) {
+			let shouldClick = false
+			// let parent = (await element.$x('..'))[0]; // get parent
+			// let parentParent = (await parent.$x('..'))[0]; // get parent
+			let grandParent = (await element.$x('../..'))[0]; // get grandparent
+			let innerHTML = await (await grandParent.getProperty('innerText')).jsonValue() //get property like innerhtml from puppeteer elementHandle
+			let elementLC = innerHTML.toLowerCase()
+
+			const splitText = elementLC.split("\n");
+			let currentPrice = (() => {
+				let result = undefined;
+				let match = 'kr';
+				for (const e of splitText) {
+					let eString = e.toString();
+					if (eString.includes(match)) {
+						let regex = /\D/g;
+						let price = eString.replace(regex, "");
+						if (data.maxPrice !== undefined) {
+							if (parseInt(price) < parseInt(data.maxPrice)) {
+								result = price;
+							}
+						} else if (data.maxPrice === undefined) {
+							result = price;
+						}
+						// break;
+					}
+				}
+
+				return result;
+			})();
+
+			if (currentPrice === undefined) {
+				continue;
+			}
+
+			data.keywords.forEach(wish => {
+				wish = wish.toLowerCase()
+				if (elementLC.includes(wish)) {
+					shouldClick = true
+					return
+				}
+			});
+			data.blacklist.forEach(deny => {
+				deny = deny.toLowerCase()
+				if (deny && deny.length != 0 && elementLC.includes(deny)) {
+					shouldClick = false
+					return
+				}
+			});
+
+			if (shouldClick) {
+				element && element?.click();
+			}
+		}
+
+		//#endregion
+
+		// await page.waitForTimeout(50000)
+
+		//#region Gather and print elements
+		let list = await page.$$('.item-card-container');
+
+		let htmlList = list.map(async element => {
+			return await (await element.getProperty('outerHTML')).jsonValue()
+		});
+
+		let newList = []
+		for (const element of htmlList) {
+			let e = await element
+			newList.push(e)
+		}
+
+		//convert to jsdom elements
+		let jsdoms = []
+		newList.forEach(element => {
+			jsdoms.push(new JSDOM(element))
+		});
+
+		let wishButtons = []
+
+		let infos = await jsdoms.map(element => {
+			//get info
+			let title = element.window.document.body.querySelector('a').title
+
+			if (ScraperFilter(data, title)) {
+
+			} else {
+				return
+			}
+
+			let link = linkPrefix + element.window.document.body.querySelector('a').href
+			let price = element.window.document.body.querySelector('.item-card-details-price').textContent
+			let date = element.window.document.body.querySelector('.item-card-animate-time').textContent
+			let wish = ""
+
+			let contentButton = element.window.document.body.querySelector('.mb-1')
+			let contentInnerHtml = contentButton.innerHTML
+			if (contentInnerHtml.includes("Sparad i minneslistan")) {
+				wish = "YES"
+			} else if (contentInnerHtml.includes("Spara i minneslistan")) {
+				wish = "NO"
+				let buttonJSDOMElement = new JSDOM(contentInnerHtml)
+				let button = buttonJSDOMElement.window.document.body.querySelector('[aria-label="Spara i minneslistan"]')
+
+				wishButtons.push(button)
+			}
+
+			return new InfoElement(title, link, price, wish, date)
+		});
+
+		infos = infos.filter(x => x !== undefined);
+		//#endregion
+
+		console.log("--infos--")
+		console.log(`Total: ${infos.length}`)
+
+		console.log(infos)
+		for (let i = 0; i < infos.length; i++) {
+			result += infos[i]?.toString();
+		}
+
+		return infos;
+	}
+
+	static PrintScrapeStart() {
 		console.log("")
 		console.log("")
 		console.log("")
@@ -35,171 +180,6 @@ export default class Scraper {
 		console.log("")
 		console.log("")
 	}
-
-	async Scrape() {
-		this.printStart()
-		let page = this.page
-		let result = ''
-
-		for (const data of datas.list) {
-
-			if (data.ignore === "true") {
-				continue;
-			}
-
-			let url = this.getURL(data.searchterm, actionType);
-			console.log(url)
-
-			await ScraperUtils.setCookiesInBrowser(page)
-			await page.goto(url);
-			await page.waitForSelector('.site-pagename-SearchResults ');
-
-			//await page.waitForTimeout(50000)
-
-			await ScraperUtils.removeGDPRPopup(page)
-
-			//page down
-			for (let i = 0; i < 20; i++) {
-				await page.keyboard.press("PageDown");
-				await page.waitForTimeout(50)
-			}
-
-			//#region click wishbutton
-			let result = await page.$$('[aria-label="Spara i minneslistan"]');
-
-			for (const element of result) {
-				let shouldClick = false
-				// let parent = (await element.$x('..'))[0]; // get parent
-				// let parentParent = (await parent.$x('..'))[0]; // get parent
-				let grandParent = (await element.$x('../..'))[0]; // get grandparent
-				let innerHTML = await (await grandParent.getProperty('innerText')).jsonValue() //get property like innerhtml from puppeteer elementHandle
-				let elementLC = innerHTML.toLowerCase()
-
-				const splitText = elementLC.split("\n");
-				let currentPrice = (() => {
-					let result = undefined;
-					let match = 'kr';
-					for (const e of splitText) {
-						let eString = e.toString();
-						if (eString.includes(match)) {
-							let regex = /\D/g;
-							let price = eString.replace(regex, "");
-							if (data.maxPrice !== undefined) {
-								if (parseInt(price) < parseInt(data.maxPrice)) {
-									result = price;
-								}
-							} else if (data.maxPrice === undefined) {
-								result = price;
-							}
-							// break;
-						}
-					}
-
-					return result;
-				})();
-
-				if (currentPrice === undefined) {
-					continue;
-				}
-
-				data.keywords.forEach(wish => {
-					wish = wish.toLowerCase()
-					if (elementLC.includes(wish)) {
-						shouldClick = true
-						return
-					}
-				});
-				data.blacklist.forEach(deny => {
-					deny = deny.toLowerCase()
-					if (deny && deny.length != 0 && elementLC.includes(deny)) {
-						shouldClick = false
-						return
-					}
-				});
-
-				if (shouldClick) {
-					element && element?.click();
-				}
-			}
-
-			//#endregion
-
-			// await page.waitForTimeout(50000)
-
-			//#region Gather and print elements
-			let list = await page.$$('.item-card-container');
-
-			let htmlList = list.map(async element => {
-				return await (await element.getProperty('outerHTML')).jsonValue()
-			});
-
-			let newList = []
-			for (const element of htmlList) {
-				let e = await element
-				newList.push(e)
-			}
-
-			//convert to jsdom elements
-			let jsdoms = []
-			newList.forEach(element => {
-				jsdoms.push(new JSDOM(element))
-			});
-
-			let wishButtons = []
-
-			let infos = await jsdoms.map(element => {
-				//get info
-				let title = element.window.document.body.querySelector('a').title
-
-				if (ScraperFilter(data, title)) {
-
-				} else {
-					return
-				}
-
-				let link = linkPrefix + element.window.document.body.querySelector('a').href
-				let price = element.window.document.body.querySelector('.item-card-details-price').textContent
-				let date = element.window.document.body.querySelector('.item-card-animate-time').textContent
-				let wish = ""
-
-				let contentButton = element.window.document.body.querySelector('.mb-1')
-				let contentInnerHtml = contentButton.innerHTML
-				if (contentInnerHtml.includes("Sparad i minneslistan")) {
-					wish = "YES"
-				} else if (contentInnerHtml.includes("Spara i minneslistan")) {
-					wish = "NO"
-					let buttonJSDOMElement = new JSDOM(contentInnerHtml)
-					let button = buttonJSDOMElement.window.document.body.querySelector('[aria-label="Spara i minneslistan"]')
-
-					wishButtons.push(button)
-				}
-
-				return new InfoElement(title, link, price, wish, date)
-			});
-
-			infos = infos.filter(x => x !== undefined);
-			//#endregion
-
-			console.log("--infos--")
-			console.log(`Total: ${infos.length}`)
-
-			console.log(infos)
-			for (let i = 0; i < infos.length; i++) {
-				result += infos[i]?.toString();
-			}
-		}
-		console.log("--DONE--")
-
-		// fs.writeFile("newData.txt", result, function (err) {
-		// 	if (err) {
-		// 		console.log(err);
-		// 	}
-		// });
-
-		return result
-	}
-
-
 }
 
 
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import ScraperPage from './ScraperPage.js';
 import Scraper from './Scraper.js';
 import ScraperLogin from './ScraperLogin.js';
+import datas from './data/data-test.json' assert {type: 'json'};
 
 // -- LOGIN --
 async function login() {
@@ -11,7 +12,7 @@ async function login() {
         var slogin = new ScraperLogin(page)
         await slogin.Login()
     } catch (error) {
-        
+
     } finally {
         // await page.close();
         await scraperPage.CloseBrowser()
@@ -19,28 +20,29 @@ async function login() {
 }
 
 //Scraper
-
-async function runScraper() {
-    var scraperPage = new ScraperPage()
-    // var page = await scraperPage.GeneratePage(false, false)
-    var page = await scraperPage.GeneratePage(true, false)
-
-    // page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
-
-    var scraper = new Scraper(page)
-    await scraper.Scrape()
-
-    await scraperPage.CloseBrowser()
+async function runScraper(inputData, inputPage) {
+    var scraper = new Scraper()
+    await scraper.Scrape(inputData, inputPage)
 }
 
+async function StartScraper() {
+    Scraper.PrintScrapeStart();
+    // page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+
+    var scraperPage = new ScraperPage()
+    var inputPage = await scraperPage.GeneratePage(true, false)
+    for (const data of datas.list) {
+        let result = await runScraper(data, inputPage);
+    }
+    await scraperPage.CloseBrowser()
+
+    console.log("--DONE--")
+    return true
+}
+
+
 await login()
-await runScraper()
+await StartScraper()
 
-//2nd time
 
-// var page = await scraperPage.GeneratePage(false, false)
 
-// var scraper = new Scraper(page)
-// await scraper.Scrape()
-
-// await scraperPage.CloseBrowser()
